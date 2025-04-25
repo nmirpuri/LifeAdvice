1b3deb76da4485d457dd488af5066bdd0136f796
Nikhil Mirpuri <ndmirpuri@gmail.com>
2025-04-25 15:59:49 -0500

    Update life_advice_app.py

--- life_advice_app.py
+++ life_advice_app.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pandas as pd
-!pip install transformers
 from transformers import DistilBertTokenizer
 from transformers import DistilBertModel
 from torch.utils.data import Dataset, DataLoader
